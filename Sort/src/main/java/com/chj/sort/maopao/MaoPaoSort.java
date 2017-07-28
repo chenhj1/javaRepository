b7481883769c352fc91e3560483c94e11c465361
陈浩杰 <chenhj1@ifeng.com>
2017-07-28 09:57:27 +0800

    冒泡排序 快排

---
+++ Sort/src/main/java/com/chj/sort/maopao/MaoPaoSort.java
@@ -0,0 +1,29 @@
+package com.chj.sort.maopao;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.junit.Test;
+
+/**
+ * Created by chenhj1 on 2017/7/27.
+ */
+public class MaoPaoSort {
+
+    @Test
+    public void sort() {
+        int[] a = new int[]{6, 4, 1, 9, 3, 2};
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+            System.out.println("第" + i + "轮, " + JSONArray.fromObject(a).toString());
+        }
+
+        System.out.println("\n结果：" + JSONArray.fromObject(a).toString());
+
+    }
+}
---
+++ Sort/src/main/java/com/chj/sort/quicksort/QuickSort.java
@@ -0,0 +1,41 @@
+package com.chj.sort.quicksort;
+
+import net.sf.json.JSONArray;
+
+/**
+ * Created by chenhj1 on 2017/7/27.
+ */
+public class QuickSort {
+
+
+    public static int getMiddle(int[] list, int low, int high) {
+        int tmp = list[low];    //数组的第一个作为中轴
+        while (low < high) {
+            while (low < high && list[high] > tmp) {
+                high--;
+            }
+            list[low] = list[high];   //比中轴小的记录移到低端
+            while (low < high && list[low] < tmp) {
+                low++;
+            }
+            list[high] = list[low];   //比中轴大的记录移到高端
+        }
+        list[low] = tmp;              //中轴记录到尾
+        return low;                   //返回中轴的位置
+    }
+
+    public static void quickSort(int[] list, int low, int high) {
+        if (low < high) {
+            int middle = getMiddle(list, low, high);  //将list数组进行一分为二
+            quickSort(list, low, middle - 1);        //对低字表进行递归排序
+            quickSort(list, middle + 1, high);       //对高字表进行递归排序
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] list = {34, 3, 53, 2, 23, 7, 14, 10};
+        quickSort(list, 0, list.length - 1);
+        System.out.println(JSONArray.fromObject(list).toString());
+    }
+
+}
