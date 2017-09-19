4f5f5fa15209e66e341c2c4837affab0631f3ba6
chenhaojie <chenhaojie@xiaomi.com>
2017-09-19 14:16:16 +0800

    测试mac idea的debug快捷键

---
+++ DataStructure/src/test/com/chj/test/Test.java
@@ -0,0 +1,30 @@
+package com.chj.test;
+
+/**
+ * a test for mac idea debug keymap
+ */
+public class Test {
+    public static void main(String []args){
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+
+        fun1();
+
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+    }
+
+    public static void fun1(){
+        System.out.println("fun1");
+        fun2();
+    }
+
+    public static void fun2(){
+        System.out.println("fun2");
+    }
+
+}
