747a4c43b248d53c3e74aada60bee58a81b8a440
chenhaojie <chenhaojie@bytedance.com>
2020-01-21 11:49:07 +0800

    复习查找二叉树和为某个值的所有路径

--- DataStructure/src/main/java/com/chj/datastructure/Tree/TreeSub.java
+++ DataStructure/src/main/java/com/chj/datastructure/Tree/TreeSub.java
@@ -1,11 +1,10 @@
 package com.chj.datastructure.Tree;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 /**
- *  查找二叉树和为某个值的所有路径
- *
+ * 查找二叉树和为某个值的所有路径
+ * <p>
  * Created by chenhaojie on 2018/06/05.
  */
 public class TreeSub {
@@ -18,42 +17,40 @@ public class TreeSub {
      * @param expectedSum 要求的路径和
      */
     public static void findPath(TreeNode root, int expectedSum) {
-        // 创建一个链表，用于存放根结点到当前处理结点的所经过的结点
-        List<Integer> list = new ArrayList<>();
+        // 用于存放根结点到当前处理结点的所经过的结点
+        Stack<Integer> stack = new Stack<>();
         // 如果根结点不为空，就调用辅助处理方法
         if (root != null) {
-            findPath(root, 0, expectedSum, list);
+            findPath(root, 0, expectedSum, stack);
         }
     }
+
     /**
      * @param root        当前要处理的结点
      * @param curSum      当前记录的和（还未加上当前结点的值）
      * @param expectedSum 要求的路径和
-     * @param result      根结点到当前处理结点的所经过的结点，（还未包括当前结点）
+     * @param stack      根结点到当前处理结点的所经过的结点，（还未包括当前结点）
      */
-    public static void findPath(TreeNode root, int curSum, int expectedSum, List<Integer> result) {
+    public static void findPath(TreeNode root, int curSum, int expectedSum, Stack<Integer> stack) {
         // 如果结点不为空就进行处理
         if (root != null) {
             // 加上当前结点的值
             curSum += root.getValue();
-            // 将当前结点入队
-            result.add(root.getValue());
+            // 将当前结点值入栈
+            stack.add(root.getValue());
             // 如果当前结点的值小于期望的和
             if (curSum < expectedSum) {
                 // 递归处理左子树
-                findPath(root.getLeft(), curSum, expectedSum, result);
+                findPath(root.getLeft(), curSum, expectedSum, stack);
                 // 递归处理右子树
-                findPath(root.getRight(), curSum, expectedSum, result);
-            }
-            // 如果当前和与期望的和相等
-            else if (curSum == expectedSum) {
-                // 当前结点是叶结点，则输出结果
-                if (root.getLeft() == null && root.getRight() == null) {
-                    System.out.println(result);
+                findPath(root.getRight(), curSum, expectedSum, stack);
+            } else if (curSum == expectedSum) {  // 如果当前和与期望的和相等
+                if (root.getLeft() == null && root.getRight() == null) { // 当前结点是叶结点，则输出结果
+                    System.out.println(stack);
                 }
             }
-            // 移除当前结点
-            result.remove(result.size() - 1);
+            // 当前节点值出栈
+            stack.pop();
         }
     }
 
@@ -67,6 +64,6 @@ public class TreeSub {
      */
     public static void main(String[] args) {
         TreeNode root = TreeTraversal.createTree();
-        findPath(root,20);
+        findPath(root, 20);
     }
 }
